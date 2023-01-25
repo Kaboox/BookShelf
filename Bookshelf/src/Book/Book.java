be936717b33c5a151ed91be8d26f550b83aa6cc8
Kacper Smietana <kabox9@gmail.com>
2023-01-25 22:20:23 +0100

    added first demo

---
+++ Bookshelf/src/Book/Book.java
@@ -0,0 +1,69 @@
+package Book;
+
+import java.util.Scanner;
+
+
+public class Book {
+    static Scanner scan = new Scanner(System.in);
+    private String title;
+    private String author;
+    private String genre;
+    private boolean completed;
+    private int rating;
+
+    // constructors
+    public Book() {};
+    public Book(String title, String author, String genre, boolean completed) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.completed = completed;
+    }
+    public Book(String title, String author, String genre, boolean completed, int rating) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.completed = completed;
+        this.rating = rating;
+    }
+
+    // getters
+    public String getTitle() {
+        return this.title;
+    }
+    public String getAuthor() {
+        return this.author;
+    }
+    public String getGenre() {
+        return this.genre;
+    }
+    public boolean getCompleted() {
+        return this.completed;
+    }
+    public int getRating() {
+        return this.rating;
+    }
+
+    // setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Title: " + getTitle() + ", author: " + getAuthor() + ", genre: " + getGenre() + ", completion status: " + getCompleted() + ", rating: " + getRating();
+    }
+}
---
+++ Bookshelf/src/main/Main.java
@@ -0,0 +1,182 @@
+package main;
+
+import Book.Book;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+// Bookshelf contains shelf with books, you can adjust it to your liking
+
+public class Main {
+   // static Book[] books = new Book[5];
+    static ArrayList<Book> booklist = new ArrayList();
+    static Scanner scan = new Scanner(System.in);
+    static public String start() {
+        String choice;
+        System.out.println("Welcome to your Bookshelf, what would you like to do?");
+        System.out.println("A - add new object to your shelf");
+        System.out.println("S - display all present items");
+        System.out.println("D - delete an item");
+        System.out.println("M - modify item's properties");
+        System.out.println("F - display all of the object's properties");
+        System.out.println("Q - quit");
+        choice = scan.next();
+        return choice;
+    }
+    // methods to get user's input values for creating new item
+    public static void askTitle(){
+            System.out.println("Pass in the book's title");
+            String title = scan.next();
+            //System.out.print(title);
+            askAuthor(title);
+
+    }
+    public static void askAuthor(String title) {
+        System.out.println("Pass in the book's author");
+        String author = scan.next();
+        askGenre(title, author);
+    }
+    public static void askGenre(String title, String author) {
+        System.out.println("Pass in the book's genre");
+        String genre = scan.next();
+        askCompleted(title, author, genre);
+
+    }
+    public static void askCompleted(String title, String author, String genre) {
+        boolean completion;
+        System.out.println("Pass in y/n, regarding if you have finished the book already");
+        String choice = scan.next();
+        char char_choice = choice.toLowerCase().charAt(0);
+        if (char_choice == 'n') {
+            completion = false;
+            //books[0] = new Book(title, author, genre, completion);
+            booklist.add(new Book(title, author, genre, completion));
+        } else if (char_choice == 'y') {
+            completion = true;
+            askRating(title, author, genre, completion);
+        }
+    }
+    public static void askRating(String title, String author, String genre, boolean completion) {
+        System.out.println("What do you rate the book? 0-10");
+        int rating = scan.nextInt();
+        booklist.add(new Book(title, author, genre, completion, rating));
+    }
+    public static void displayArray(ArrayList<Book> booklist) {
+        System.out.print("[ ");
+        for (Book book : booklist) {
+            if (book instanceof Book) {
+                System.out.print(book.getTitle());
+                System.out.print(", ");
+            } else {
+                System.out.print(" ");
+                System.out.print(", ");
+            }
+
+        }
+        System.out.println(" ]");
+    }
+    // asks user to choose object to modify later with changeProperties method
+    public static void chooseObject() {
+        System.out.println("Which object would you like to modify? (0-4)");
+        displayArray(booklist);
+        System.out.println(" ");
+        int chosenIndex = scan.nextInt();
+        changeProperties(chosenIndex);
+    }
+    // changes chosen properties of the book
+    public static void changeProperties(int index) {
+        System.out.println("What property would you like to change? ");
+        System.out.println("T - title");
+        System.out.println("A - author");
+        System.out.println("G - genre");
+        System.out.println("C - completion");
+        System.out.println("R - rating");
+        String strChoice = scan.next();
+        char choice = strChoice.toLowerCase().charAt(0);
+        if (choice == 't') {
+            System.out.println("Put in the new title");
+            String title = scan.next();
+            booklist.get(index).setTitle(title);
+        } else if (choice == 'a') {
+            System.out.println("Put in the new author");
+            String author = scan.next();
+            booklist.get(index).setAuthor(author);
+        } else if (choice == 'g') {
+            System.out.println("Put in the new genre");
+            String genre = scan.next();
+            booklist.get(index).setGenre(genre);
+        } else if (choice == 'c') {
+            System.out.println("Put in the new completion status");
+            String completion = scan.next();
+            if (completion.toLowerCase().charAt(0) == 'y') {
+                booklist.get(index).setCompleted(true);
+            } else if (completion.toLowerCase().charAt(0) == 'n') {
+                booklist.get(index).setCompleted(false);
+            }
+        } else if (choice == 'r') {
+            System.out.println("Put in the new rating");
+            int rating = scan.nextInt();
+            booklist.get(index).setRating(rating);
+        }
+    }
+    // displays all properties of the given object
+    public static void allInfo() {
+        System.out.println("Choose the item you wanna display");
+        //displayArray(books);
+        int index = scan.nextInt();
+        System.out.println(booklist.get(index));
+    }
+    // deleted book from the shelf
+    public static void deleteBook() {
+        System.out.println("Which item would you like to delete?");
+        int index = scan.nextInt();
+        booklist.remove(index);
+    }
+    public static void quit() {
+        scan.close();
+    }
+
+
+    public static void main(String[] args) {
+        char choice;
+        booklist.add(new Book("Tadeusz12", "Zbigniew","Dramat",true,6));
+        booklist.add(new Book("Tadeusz1251", "Zbigniew","Dramat",true,6));
+
+        boolean valid_option = false;
+        do {
+            do {
+                choice = start().toLowerCase().charAt(0);
+                if (choice != 's' && choice != 'a' && choice != 'd' && choice != 'm' && choice != 'f' && choice != 'q') {
+                    System.out.println("Wrong input, try again");
+                    valid_option = false;
+                } else
+                    valid_option = true;
+            } while (valid_option != true);
+
+            if (choice == 'a') {
+                askTitle();
+                displayArray(booklist);
+
+            } else if (choice == 's') {
+                displayArray(booklist);
+            } else if (choice == 'm') {
+                chooseObject();
+
+            } else if (choice == 'f') {
+                displayArray(booklist);
+                allInfo();
+            } else if (choice == 'd') {
+                displayArray(booklist);
+                deleteBook();
+                displayArray(booklist);
+            } else if (choice == 'q') {
+                quit();
+                //break;
+            }
+        } while (choice != 'q');
+
+    }
+    // CHANGE EVERYTHING TO WORK WITH ARRAY LIST
+}
