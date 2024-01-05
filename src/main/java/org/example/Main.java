0215dc2ff90fdf096c85b6d60dff1b7e3ae9188f
Kacper Smietana <kabox9@gmail.com>
2024-01-05 09:02:13 +0100

    view options initialization

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Hello welcome to your private BookShelf. What would you like to do?");
+        viewOptions();
+
+    }
+
+    public static void viewOptions() {
+
     }
 }
