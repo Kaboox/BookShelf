6e0ded96e207fe0d2626a86fea76e1a74ecb68bf
Kacper Smietana <kabox9@gmail.com>
2024-02-06 15:15:26 +0100

    comments with ideas and continuous development

--- src/main/java/org/example/Bookshelf.java
+++ src/main/java/org/example/Bookshelf.java
@@ -7,18 +7,26 @@ public class Bookshelf {
     private ArrayList<Book> books  = new ArrayList<Book>();
 
     public static void viewOptions() {
+
         System.out.println("A - add new book");
         System.out.println("V - view all books");
         System.out.println("D - delete selected book");
         System.out.println("M - view more details about a book");
         System.out.println("Q - close");
+
     }
 
     public void addBook() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Pass in the book's title: ");
         String title = scan.nextLine();
+        System.out.println("Pass in the book's author: ");
+        String author = scan.nextLine();
+        System.out.println("Pass in the book's genre: ");
+        String genre = scan.nextLine();
+        System.out.println("Pass in the books's release year: ");
+        int release_year = scan.nextInt();
 
-        books.add(new Book(title));
+        books.add(new Book(title, author, genre, release_year));
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,13 @@ import org.example.Bookshelf.*;
 public class Main {
     public static void main(String[] args) {
 
+        // flow programu
+        // view zwroci false na q reszta dzialanie i true
+        boolean working = true;
         System.out.println("Hello and welcome to your private BookShelf. What would you like to do?");
-        Bookshelf.viewOptions();
+        while(working) {
+            Bookshelf.viewOptions();
+        }
 
     }
 
